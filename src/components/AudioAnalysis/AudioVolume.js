9e0e3d6585320077dc1fd9258582460f2f3def96
peter-suggate <petersuggate@gmail.com>
2016-05-23 08:16:22 +1200

    Show a rudimentary height bar for the current audio volume.

--- src/components/AudioAnalysis/AudioVolume.js
+++ src/components/AudioAnalysis/AudioVolume.js
@@ -5,10 +5,49 @@ export default class AudioVolume extends Component {
     volume: PropTypes.number
   }
 
-  render() {
-    const {volume} = this.props;
+  componentDidMount() {
+    const context = this.refs.canvas.getContext('2d');
+    this.paint(context);
+  }
 
-    return (<div>Mic volume: {volume}</div>);
+  componentDidUpdate() {
+    const context = this.refs.canvas.getContext('2d');
+    context.clearRect(0, 0, 200, 200);
+    this.paint(context);
+  }
+
+  static attributes = {
+    totalTimeDisplaySeconds: 10,
+    volumeMax: 150.0,
+    volumeMin: 0.0
+  }
+
+  linearInterpolate(val, fromMin, fromMax, toMin, toMax) {
+    const tmp = (val - fromMin) / (fromMax - fromMin);
+    return (tmp + toMin) * (toMax - toMin);
+  }
+
+  paint(context) {
+    context.save();
+
+    const {volume} = this.props;
+    const height = this.linearInterpolate(
+      volume,
+      AudioVolume.attributes.volumeMin, AudioVolume.attributes.volumeMax,
+      0, 200
+      );
+    // context.translate(100, 100);
+    // context.rotate(this.props.volume, 100, 100);
+    context.fillStyle = '#F00';
+    context.fillRect(0, 0, 200, 200 - height);
+    context.restore();
+  }
+
+  render() {
+    // const {volume} = this.props;
+
+    return (<canvas width={200} height={200} ref="canvas" />);
+    // return (<div>Mic volume: {volume}</div>);
   }
 }
 
