69f1797b260035eeaa0ecf09436e538cde424594
peter-suggate <petersuggate@gmail.com>
2016-05-26 23:40:37 +1200

    Remove a whole lot of unneeded example code

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,10 @@
 /**
  *  Point of contact for component modules
  *
- *  ie: import { CounterButton, InfoBar } from 'components';
+ *  ie: import { MusicNotationPanel, AudioVolume } from 'components';
  *
  */
 
-export CounterButton from './CounterButton/CounterButton';
-export GithubButton from './GithubButton/GithubButton';
-export InfoBar from './InfoBar/InfoBar';
-export MiniInfoBar from './MiniInfoBar/MiniInfoBar';
-export SurveyForm from './SurveyForm/SurveyForm';
-export WidgetForm from './WidgetForm/WidgetForm';
 export MusicNotationPanel from './MusicNotationPanel/MusicNotationPanel';
 export AudioVolume from './AudioAnalysis/AudioVolume';
 export AudioPitch from './AudioAnalysis/AudioPitch';
