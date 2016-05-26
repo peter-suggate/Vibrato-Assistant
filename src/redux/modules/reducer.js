29bb13e217b462f5477ceb9c287772d77c42e477
peter-suggate <petersuggate@gmail.com>
2016-05-26 23:40:45 +1200

    Fix build

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,26 +1,11 @@
 import { combineReducers } from 'redux';
-import multireducer from 'multireducer';
 import { routerReducer } from 'react-router-redux';
-import {reducer as reduxAsyncConnect} from 'redux-async-connect';
+import { reducer as reduxAsyncConnect } from 'redux-async-connect';
 
-import auth from './auth';
-import counter from './counter';
-import {reducer as form} from 'redux-form';
-import info from './info';
-import widgets from './widgets';
 import audioRec from './audioRecorder';
 
 export default combineReducers({
-  audioRecorder: audioRec,
-  routing: routerReducer,
   reduxAsyncConnect,
-  auth,
-  form,
-  multireducer: multireducer({
-    counter1: counter,
-    counter2: counter,
-    counter3: counter
-  }),
-  info,
-  widgets
+  routing: routerReducer,
+  audioRecorder: audioRec
 });
