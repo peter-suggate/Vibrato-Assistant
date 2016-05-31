827842488c5f9d657f3ea237f5510b4a4dc47056
peter-suggate <petersuggate@gmail.com>
2016-06-01 00:12:18 +1200

    Add tests for the variable recorder, fixing bugs

--- src/helpers/Audio/VariablePeriodNoteRecorder.js
+++ src/helpers/Audio/VariablePeriodNoteRecorder.js
@@ -1,8 +1,7 @@
 // Receives a steady stream of pitches from the client (at a highish sampling
 // rate) and spits out notes of variable length.
 export default class VariablePeriodNoteRecorder {
-  constructor(noteBpm) {
-    this.noteBpm = noteBpm;
+  constructor() {
     this.pitchBuffer = [];
     this.latestNote = null;
     this.currentNoteStartTime = null;
@@ -11,29 +10,25 @@ export default class VariablePeriodNoteRecorder {
   start() {
     this.firstNoteStartTime = Date.now();
     this.currentNoteStartTime = this.firstNoteStartTime;
+    this.noteDurationMsec = 0;
   }
 
   timeAfterStartMsec() {
     return Date.now() - this.firstNoteStartTime;
   }
 
-  addCurrentPitch(pitch) {
-    this.pitchBuffer.push(pitch);
+  addCurrentPitch(pitch, quantaDurationMsec) {
+    const ret = this._addCurrentPitch(pitch);
 
-    if (this._detectedEndOfNote()) {
-      const noteDurationMsec = Date.now() - this.currentNoteStartTime;
-      this.latestNote = {
-        notePitch: this._calculateRepresentativeNotePitch(),
-        startTimeMsec: this.currentNoteStartTime - this.firstNoteStartTime,
-        durationMsec: noteDurationMsec
-      };
-
-      this.pitchBuffer = [];
-      this.currentNoteStartTime += noteDurationMsec;
-      return true;
+    if (quantaDurationMsec !== 'undefined') {
+      // Pass a valid quantaDurationMsec when driving the note recording process offline.
+      this.noteDurationMsec += quantaDurationMsec;
+    } else {
+      // Pass in no quantaDurationMsec when driving the note recording process online (in "real time").
+      this.noteDurationMsec = Date.now() - this.currentNoteStartTime;
     }
 
-    return false;
+    return ret;
   }
 
   stop() {
@@ -46,18 +41,38 @@ export default class VariablePeriodNoteRecorder {
 
   // Private methods
 
-  _detectedEndOfNote() {
+  _addCurrentPitch(pitch) {
+    const logPitch = this._toInternalPitchRep(pitch);
+    if (this._detectedEndOfNote(logPitch)) {
+      // const noteDurationMsec = Date.now() - this.currentNoteStartTime;
+      this.latestNote = {
+        notePitch: this._toPitchInHz(this._calculateRepresentativeNotePitch()),
+        startTimeMsec: this.currentNoteStartTime - this.firstNoteStartTime,
+        durationMsec: this.noteDurationMsec
+      };
+
+      this.pitchBuffer = [logPitch];
+      this.currentNoteStartTime += this.noteDurationMsec;
+      this.noteDurationMsec = 0;
+      return true;
+    }
+
+    this.pitchBuffer.push(logPitch);
+    return false;
+  }
+
+  _detectedEndOfNote(logPitch) {
     const {pitchBuffer} = this;
     const numPitches = pitchBuffer.length;
 
-    if (numPitches <= 1) {
+    if (numPitches === 0) {
       return false; // Not enough pitches yet.
     }
 
     // For now, rudimentary scheme that checks the most recent pitch against
     // its previous.
-    const mostRecentPitch = Math.log2(pitchBuffer[numPitches - 1]);
-    const previousPitch = Math.log2(pitchBuffer[numPitches - 2]);
+    const mostRecentPitch = logPitch;
+    const previousPitch = pitchBuffer[numPitches - 1];
     const ratio = mostRecentPitch / previousPitch;
     let delta = 0;
     if (ratio >= 1) {
@@ -79,5 +94,17 @@ export default class VariablePeriodNoteRecorder {
     });
     total /= this.pitchBuffer.length;
     return total;
+  }
+
+  _round2dp(number) {
+    return Math.round(number * 100) / 100;
+  }
+
+  _toInternalPitchRep(pitchInHz) {
+    return Math.log2(pitchInHz);
+  }
+
+  _toPitchInHz(pitchInternalRep) {
+    return this._round2dp(Math.pow(2, pitchInternalRep));
   }
 }
---
+++ src/helpers/Audio/__tests__/VariablePeriodNoteRecorder-test.js
@@ -0,0 +1,50 @@
+import { expect } from 'chai';
+import VariablePeriodNoteRecorder from 'helpers/Audio/VariablePeriodNoteRecorder';
+
+describe('VariablePeriodNoteRecorder', () => {
+  describe('addCurrentPitch', () => {
+    const noteRecorder = new VariablePeriodNoteRecorder();
+    it('returns false when called in presence of only a single pitch', () => {
+      expect(noteRecorder.addCurrentPitch(440)).to.equal(false);
+    });
+
+    it('returns false when called on two duplicate pitches added', () => {
+      expect(noteRecorder.addCurrentPitch(440)).to.equal(false);
+    });
+
+    it('returns true when called after a distinct pitch is added', () => {
+      expect(noteRecorder.addCurrentPitch(660)).to.equal(true);
+    });
+  });
+
+  describe('getLatestNote', () => {
+    const noteRecorder = new VariablePeriodNoteRecorder();
+    noteRecorder.start();
+
+    const noteDuration = 1;
+    const longNoteDuration = 5;
+    it('returns the first note when a new pitch is added', () => {
+      expect(noteRecorder.addCurrentPitch(440, noteDuration)).to.equal(false);
+      expect(noteRecorder.addCurrentPitch(660, longNoteDuration)).to.equal(true);
+      const note = noteRecorder.getLatestNote();
+      expect(note.notePitch).to.equal(440);
+    });
+
+    let note = null;
+    it('returns the second note when a new pitch is added', () => {
+      expect(noteRecorder.addCurrentPitch(220, noteDuration)).to.equal(true);
+      note = noteRecorder.getLatestNote();
+      expect(note.notePitch).to.equal(660);
+    });
+
+    it('returns correct note time and duration information', () => {
+      expect(note.durationMsec).to.equal(longNoteDuration);
+      expect(note.startTimeMsec).to.equal(1);
+
+      expect(noteRecorder.addCurrentPitch(440, noteDuration)).to.equal(true);
+      note = noteRecorder.getLatestNote();
+      expect(note.durationMsec).to.equal(noteDuration);
+      expect(note.startTimeMsec).to.equal(6);
+    });
+  });
+});
