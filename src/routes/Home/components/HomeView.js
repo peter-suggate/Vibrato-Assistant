83b7c18e906b98f053d4a00f02871468dc55ee3d
peter-suggate <petersuggate@gmail.com>
2016-07-29 07:49:47 +1200

    Remove boilerplate UI

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
-import classes from './HomeView.scss'
+// import classes from './HomeView.scss'
 import { containers } from 'modules/real-time-audio'
 
 export const HomeView = () => {
   const { BasicRealtimeAudioDisplay } = containers
 
   return <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className={classes.duck}
-      src={DuckImage} />
     <BasicRealtimeAudioDisplay />
   </div>
 }
